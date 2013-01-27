1010139aa68cc224e929c4f0d0b2c109af4f3c4c
Megan Davis <megan.davis@oberlin.edu>
2013-01-27 15:14:47 -0600

    Finished sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 
   # Validate that there is, indeed, something in name/email
   validates :name, :presence => true, :length => {:maximum => 50}
@@ -22,5 +23,11 @@ class User < ActiveRecord::Base
   validates :email,:uniqueness => {:case_sensitive => false}, :presence => true, :format => { :with => VALID_EMAIL_REGEX } 
   validates :password, :presence => true, :length => {:minimum => 6}
   validates :password_confirmation, :presence => true
+
+  private
+
+    def create_remember_token
+        self.remember_token = SecureRandom.base64.tr("+/", "-_")
+    end
 
 end
